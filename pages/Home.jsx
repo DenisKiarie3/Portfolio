541d6fd8e26c8384adb2fc5425da84e53912643c
DenisKiarie3 <deniskiarie3@gmail.com>
2025-06-24 18:20:46 +0300

    Added a My Skills Section

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,6 +3,7 @@ import { StarBackground } from "../components/StarBackground.jsx"
 import { Navbar } from "../components/Navbar.jsx"
 import { HeroSection } from "../components/HeroSection.jsx"
 import { AboutSection } from "../components/AboutSection.jsx"
+import { SkillsSection } from "../components/SkillsSection.jsx"
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme Toggle */}
@@ -15,6 +16,7 @@ export const Home = () => {
         <main>
             <HeroSection/>
             <AboutSection/>
+            <SkillsSection/>
         </main>
 
         {/*Footer */}
