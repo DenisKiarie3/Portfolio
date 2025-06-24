c9702462c0e01e84133dd35eb746d5b0b5cfd660
DenisKiarie3 <deniskiarie3@gmail.com>
2025-06-24 11:53:44 +0300

    Added background effects

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,10 +1,11 @@
 import { ThemeToggle } from "../components/ThemeToggle.jsx"
+import { StarBackground } from "../components/StarBackground.jsx"
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme Toggle */}
         <ThemeToggle/>
         {/* Background Effects */}
-
+        <StarBackground/>
         {/* Navbar */}
 
         {/* Main Content */}
