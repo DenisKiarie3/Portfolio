e98c1c394cb48f03802d00d1943f5c8b5a0c176e
DenisKiarie3 <deniskiarie3@gmail.com>
2025-06-25 15:05:40 +0300

    Updated about section

--- components/AboutSection.jsx
+++ components/AboutSection.jsx
@@ -39,7 +39,7 @@ export const AboutSection = () => {
                             </a>
 
                             <a 
-                            href="/Denis-Kiarie-CV2.pdf" 
+                            href="/Denis-Kiarie-CV.pdf" 
                             download
                             className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                                 Download CV
